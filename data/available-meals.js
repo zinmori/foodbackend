8113e8b3bbf88aff66d174f8ebe8300667226c4e
Ezechiel AGBAN <ezechiel9087@gmail.com>
2024-03-27 16:30:54 +0000

    img object

--- data/available-meals.js
+++ data/available-meals.js
@@ -1,10 +1,12 @@
+const macAndCheese = require('../public/images/mac-and-cheese.jpg');
+
 const mealsData = [
   {
     "id": "m1",
     "name": "Mac & Cheese",
     "price": "8.99",
     "description": "Creamy cheddar cheese mixed with perfectly cooked macaroni, topped with crispy breadcrumbs. A classic comfort food.",
-    "image": "https://foodbackend-z.vercel.app/images/mac-and-cheese.jpg"
+    "image": {macAndCheese}
   },
   {
     "id": "m2",
